6381efec017e74942579fd5d6effc4f5587fc6bd
Gaurav Tiwari <gauravtiwariyz@gmail.com>
2019-06-17 19:21:18 +0530

    end

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,35 @@
-from django.test import TestCase
-
+from django.contrib.auth import get_user_model
+from django.test import Client, TestCase
+from django.urls import reverse
+from .models import Post
 # Create your tests here.
+class SimpleDbTest(TestCase):
+	def setUp(self):
+		self.user = get_user_model().objects.create(
+			username = 'test',
+			email = 'test@test.com',
+			password = 'secret')
+		self.post = Post.objects.create(
+			title = "sample post",
+			author = self.user,
+			body = 'sample body')
+	def test_string_representation(self):
+		post = Post(title= 'sample post')
+		self.assertEqual(str(post),post.title)
+	def test_post_contents(self):
+		self.assertEqual(f'{self.post.title}', "sample post")
+		self.assertEqual(f'{self.post.author}', 'test')
+		self.assertEqual(f'{self.post.body}', 'sample body')
+	def test_list_view(self):
+		resp = self.client.get(reverse('posts'))
+		self.assertEqual(resp.status_code,200)
+		self.assertTemplateUsed(resp,'blog/list.html')
+		self.assertContains(resp,'sample body')
+	def test_detail_view(self):
+		resp = self.client.get('post/1/')
+		no_resp = self.client.get('post/200/')
+		self.assertEqual(resp.status_code,200)
+		self.assertEqual(no_resp.status_code,404)
+		self.assertTemplateUsed(resp,'blog/detail.html')
+		self.assertContains(resp,'sample post')
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.views.generic import ListView,DetailView
+from .models import Post
 # Create your views here.
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/list.html'
+	context_object_name = 'posts'
+class PostDetailView(DetailView):
+	model = Post
+	template_name = 'blog/detail.html'
+	context_object_name = 'post'
